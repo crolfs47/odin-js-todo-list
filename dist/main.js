94af2549ba3868a74ba7c9a858751f9a37245799
Cathy Rolfs <crolfs47@gmail.com>
2023-06-16 16:47:31 -0500

    Work on fixing github pages deployment

--- dist/main.js
+++ dist/main.js
@@ -486,7 +486,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _project__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./project */ \"./src/project.js\");\n/* harmony import */ var _projectDisplay__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./projectDisplay */ \"./src/projectDisplay.js\");\n\n\n\n\n(0,_project__WEBPACK_IMPORTED_MODULE_1__.setDefaultProject)();\n(0,_projectDisplay__WEBPACK_IMPORTED_MODULE_2__.showProjects)();\n\n\n//# sourceURL=webpack://odin-js-todo-list/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _project__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./project */ \"./src/project.js\");\n/* harmony import */ var _projectDisplay__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./projectDisplay */ \"./src/projectDisplay.js\");\n\n\n\n\nconsole.log('test');\n(0,_project__WEBPACK_IMPORTED_MODULE_1__.setDefaultProject)();\n(0,_projectDisplay__WEBPACK_IMPORTED_MODULE_2__.showProjects)();\n\n\n//# sourceURL=webpack://odin-js-todo-list/./src/index.js?");
 
 /***/ }),
 
